66b394203b1190066f23839ca747f5f4cf9c7795
Clayton G Ousley <cousley@organic.com>
2019-01-04 11:07:33 -0500

    Initial commit

---
+++ src/js/index.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import premiumColors from './premium';
+
+const colorBtn = document.getElementById('color');
+const premiumColorBtn = document.getElementById('premium-color');
+
+const availableColors = [
+	'aliceblue',
+	'blanchedalmond',
+	'darkorchid',
+	'darkseagreen',
+	'khaki',
+	'lightblue'
+];
+
+
+
+// window.onload = function () {
+//
+// 	colorBtn.addEventListener('click', () => {
+// 		const randIdx = Math.floor(Math.random() * availableColors.length);
+// 		document.documentElement.style.setProperty('--bg', availableColors[randIdx]);
+// 	});
+//
+// 	premiumColorBtn.addEventListener('click', () => {
+// 		const randIdx = Math.floor(Math.random() * premiumColors.length);
+// 		document.documentElement.style.setProperty('--bg', premiumColors[randIdx]);
+// 	});
+// }
